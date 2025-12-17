ee6eebc870c077fe698b135ccb8af343749e1cc6
Avinaasha382 <avinaasha382@gmail.com>
2025-12-17 20:03:27 +0530

    Port fix

--- main.py
+++ main.py
@@ -137,4 +137,6 @@ def predict():
     return render_template("layout.html", page="upload", form=form)
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
